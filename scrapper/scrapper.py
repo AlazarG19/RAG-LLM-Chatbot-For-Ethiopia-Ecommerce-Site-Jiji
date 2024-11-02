fbcfc487c564df2f77fd2a1bf34dd70e34adb20c
AlazarG19 <alazargetachew70@gmail.com>
2024-11-02 04:18:58 -0700

    feat: update requirement.txt, add required chromedriver and import necessary classes for the webscrapper

---
+++ scrapper/scrapper.py
@@ -0,0 +1,9 @@
+import time
+import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium_stealth import stealth  
+
+
