103248bcf28eb0612ddc0902598841b007186ca4
AlazarG19 <alazargetachew70@gmail.com>
2024-11-03 06:14:46 -0800

    feat: add docker file for initializing weaviate, add weaviate class, create collection, convert file to csv and upload vectors to weaviate

--- vector_db/main.py
+++ vector_db/main.py
@@ -7,32 +7,45 @@ class ProductDatabase:
         self.client = weaviate.connect_to_local(host=host, port=port, grpc_port=grpc_port)
         self.product_db = None
         self.products = []
+        self.connected = False
+        self.collection_created = False
 
     def connect(self):
-        if not self.client.is_ready():
-            raise ConnectionError("Failed to connect to Weaviate.")
-        print("Connection to Weaviate established.")
+        if not self.connected:
+            if not self.client.is_ready():
+                raise ConnectionError("Failed to connect to Weaviate.")
+            self.connected = True
+            print("Connection to Weaviate established.")
+        else:
+            print("Already connected to Weaviate.")
 
     def create_product_collection(self):
         # Create the Product collection
-        self.client.collections.create(
-            "Product",
-            description="Jiji product data",
-            vectorizer_config=None,
-            properties=[
-                Property(name="item_name", data_type=DataType.TEXT),
-                Property(name="price", data_type=DataType.TEXT),
-                Property(name="description", data_type=DataType.TEXT),
-                Property(name="location", data_type=DataType.TEXT),
-            ]
-        )
-        self.product_db = self.client.collections.get("Product")
-        print("Product collection created.")
 
-    def add_products_with_embeddings(self, chunks, get_gemini_embeddings):
+        if not self.collection_created:
+            # Create the Product collection
+            if(not self.client.collections.exists("Product")):
+                self.client.collections.create(
+                    "Product",
+                    description="Jiji product data",
+                    vectorizer_config=None,
+                    properties=[
+                        Property(name="item_name", data_type=DataType.TEXT),
+                        Property(name="price", data_type=DataType.TEXT),
+                        Property(name="description", data_type=DataType.TEXT),
+                        Property(name="location", data_type=DataType.TEXT),
+                    ]
+                )
+            self.product_db = self.client.collections.get("Product")
+            self.collection_created = True
+            print("Product collection created.")
+        else:
+            print("Product collection already exists.")
+
+    def add_products_with_embeddings(self, chunks, get_embeddings):
         # Generate embeddings and add products to the collection
         for chunk in chunks:
-            embedding = get_gemini_embeddings(chunk["Full"].to_list())
+            embedding = get_embeddings(chunk["Full"].to_list())
             chunk.reset_index(drop=True, inplace=True)
             for index, row in chunk.iterrows():
                 self.products.append({
@@ -57,7 +70,7 @@ class ProductDatabase:
                 )
         print("Products batch-inserted into the collection.")
 
-    def query_similar_products(self, user_query, query_embedding, llm):
+    def query_similar_products_prompt(self, user_query, query_embedding, llm):
         # Embed the user query and retrieve similar products
         results = self.product_db.query.near_vector(
             near_vector=query_embedding,
@@ -73,11 +86,7 @@ class ProductDatabase:
         ])
         final_prompt = f"Given the following products:\n{context}\nAnswer the user's question: {user_query}"
         
-        # Use LLM to generate an answer
-        answer = llm.invoke(final_prompt)
-        print("Generated Answer:", answer.content)
-        return answer.content
-
+        return final_prompt
     def close_connection(self):
         # Close the connection
         self.client.close()
